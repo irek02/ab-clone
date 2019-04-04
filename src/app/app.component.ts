ee94af6d37793a7b253b0dca6b5d0eef03c47439
irek02 <imirgaleev@gmail.com>
2019-04-04 18:32:34 -0400

    building the filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { FormBuilder, FormArray, FormControl, FormGroup } from '@angular/forms';
 import { DataService } from './data.service';
 
 @Component({
@@ -9,13 +10,22 @@ import { DataService } from './data.service';
 })
 export class AppComponent implements OnInit {
   title = 'airbnb-clone';
-
+  homeTypeFilters = [
+    { name: 'Entire place', description: 'Have the whole place to yourself', selected: false },
+    { name: 'Private room', description: 'Your own room with shared common spaces', selected: false },
+    { name: 'Shared room', description: 'Share a room as a common space', selected: false },
+    { name: 'Hotel room', description: 'Private or shared room in a hotel', selected: false }
+  ];
   listings$: Observable<any[]>;
+  form: FormGroup;
 
-  constructor(private dataService: DataService) {}
+  constructor(private dataService: DataService, private fb: FormBuilder) {}
 
   ngOnInit() {
     this.listings$ = this.dataService.getListings$();
+    this.form = this.fb.group({
+      homeTypeFilters: new FormArray(this.homeTypeFilters.map(filter => new FormControl(false)))
+    });
   }
 
   parseStars(stars: number) {
@@ -26,5 +36,22 @@ export class AppComponent implements OnInit {
     }
 
     return result;
+  }
+
+  submit(formValue) {
+    // figure out how to:
+    // - close the filter dropdown
+    // - make the current filter highlighted
+    // - show the loader
+    // - re-fetch the listings with the filters applied
+    // - show the new listings
+    // const formValue = Object.assign({}, value, {
+    //   skills: form.skills.map((selected, i) => {
+    //     return {
+    //       id: this.user.skills[i].id,
+    //       selected
+    //    }
+    //   });
+    // });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
