c56836ad75ba98fea3488c3d10e5693960289ed4
irek02 <imirgaleev@gmail.com>
2019-04-13 16:57:52 -0400

    added and styled navbar

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderContainerComponent } from './containers/header-container/header-container.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FiltersComponent } from './components/filters/filters.component';
 
 @NgModule({
-  declarations: [HeaderContainerComponent],
+  declarations: [HeaderContainerComponent, NavComponent, FiltersComponent],
   imports: [
     CommonModule
   ],
